3f2cf29daa30b15995aaa40c5f26760c763d2c15
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2021-07-06 10:04:51 +0300

    Delete heroicons-shims.d.ts

--- src/heroicons-shims.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@heroicons/vue/*' {
-  const content: any
-  export default content
-}
-
-declare module '@heroicons/vue/solid' {
-  export { default as ChevronDownIcon } from '@heroicons/vue/solid/esm/ChevronDownIcon'
-}
